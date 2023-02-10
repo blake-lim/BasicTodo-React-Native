ae56fcf65e2a805065c3f58173e918ff0885aa90
blake-lim <blake198492@kakao.com>
2023-02-10 15:15:26 +0900

    ADD : 리덕스 셋팅

---
+++ components/InputForm.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView'
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
+
+const InputForm = () => {
+  return (
+    <KeyboardAvoidingView   
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    style={styles.addFormContainer}>
+        <TextInput 
+        style={styles.inputField}
+        placeholder="할 일을 작성해주세요"
+        ></TextInput>
+        <Pressable style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+        </Pressable>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default InputForm
+
+const styles = StyleSheet.create({
+addFormContainer : {
+    flexDirection : 'row',
+    marginTop : 'auto',
+    marginBottom : 30,
+    paddingHorizontal : 20,
+    backgroundColor : '#f7f8fa',
+},
+inputField : {
+    flex : 1,
+    height : 42,
+    padding : 5,
+    marginRight : 25,
+    borderRadius : 4,
+    borderColor : 'rgba(0,0,0,0.2)',
+    borderWidth : 1,
+    color : '#000000',
+    fontSize : 15,
+    textAlignVertical : 'center',
+},
+addButton : {
+    justifyContent : 'center',
+    alignItems : 'center',
+    width : 42,
+    height : 42,
+    borderRadius : 4,
+    backgroundColor : 'rgba(0,0,0,0.7)',
+
+},
+addButtonText : {
+    color : 'white',
+    fontSize : 25,
+}
+
+})
